4c4fb66725ac5a6c598dd026fb0b6cd0580dae72
Thalita coelho <thalita.bastos94@gmail.com>
2024-06-25 09:21:05 -0300

    organizando as pastas

---
+++ sql/categorias.sql
@@ -0,0 +1,17 @@
+insert into categorias (descricao) values
+('Alimentação'), 
+('Assinaturas e Serviços'), 
+('Casa'), ('Mercado'), 
+('Cuidados Pessoais'), 
+('Educação'), 
+('Família'),
+('Lazer'), 
+('Pets'), 
+('Presentes'), 
+('Roupas'), 
+('Saúde'), 
+('Transporte'), 
+('Salário'), 
+('Vendas'), 
+('Outras receitas'), 
+('Outras despesas');
